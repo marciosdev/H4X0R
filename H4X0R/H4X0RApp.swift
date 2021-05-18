981dea975cbb1d3589d1682dfe2439e198cff1a8
Marcio <marcio@touchable.dev>
2021-05-17 23:18:01 -0300

    Initial Commit

---
+++ H4X0R/H4X0RApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0RApp.swift
+//  H4X0R
+//
+//  Created by Marcio Silva on 17/05/21.
+//
+
+import SwiftUI
+
+@main
+struct H4X0RApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
